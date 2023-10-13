8f33dff2c9cfa284f96a9d437c207294444f20d4
heitor lourenco <juniorio01@hotmail.com>
2023-10-13 10:41:18 -0300

    feat: create class Seller

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 import java.text.SimpleDateFormat;
-
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 
 public class App {
@@ -11,6 +12,8 @@ public class App {
 
         Department department = new Department(1, "Juridico");        
 
-        System.out.println(department);
+        Seller seller = new Seller(21, "Fulano", "Fulano@email.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
     }
 }
